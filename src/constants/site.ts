8a6e02a80ffb4f96a555981ffcf40617494c1d2a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-05-29 09:59:35 +0900

    👍 フォントを適用

---
+++ src/constants/site.ts
@@ -0,0 +1,7 @@
+export const SITE_NAME = "katasu.me (かたすみ)";
+
+export const SITE_DESCRIPTION_SHORT = "インターネットのかたすみにある、ぽつんと画像をおいておける場所。";
+
+export const SITE_DESCRIPTION_LONG = `${SITE_NAME}はインターネットのかたすみにある、画像をおいておける場所です。`;
+
+export const SITE_URL = "https://katasu.me";
