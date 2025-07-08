ad2f795aea8bb0fd505d422c67fbf0d2e5867d35
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-07-08 12:22:47 +0900

    🚧 タグページ

---
+++ src/app/(main)/user/[id]/tag/[tagname]/page.tsx
@@ -0,0 +1,10 @@
+export default function TagPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="mb-4 font-bold text-2xl">タグ検索ページ</h1>
+        <p className="text-gray-600">ここに画像を表示します。</p>
+      </div>
+    </div>
+  );
+}
