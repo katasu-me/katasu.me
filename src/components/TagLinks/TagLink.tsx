0d98a1a63cc360daf3cfc3e6f2d3956bef146434
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-07-10 21:34:13 +0900

    👍 ブラーかけてみる

--- src/components/TagLinks/TagLink.tsx
+++ src/components/TagLinks/TagLink.tsx
@@ -14,7 +14,7 @@ export default function TagLink({ name, href, image }: TagLinkProps) {
       className="relative flex h-12 w-20 shrink-0 items-center justify-center overflow-hidden rounded-md bg-warm-black-50 p-2 text-center transition-all duration-400 ease-magnetic hover:scale-105 hover:brightness-90 active:scale-95 md:h-16 md:w-24"
       href={href}
     >
-      <Image className="absolute inset-0 object-cover brightness-70" src={image} alt={`タグ: ${name}`} fill />
+      <Image className="absolute inset-0 object-cover blur-xs brightness-70" src={image} alt={`タグ: ${name}`} fill />
       <p className="z-1 text-warm-white text-xs">
         <BudouX>{name}</BudouX>
       </p>
