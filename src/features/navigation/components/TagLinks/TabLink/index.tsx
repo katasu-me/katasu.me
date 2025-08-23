a5643e60ce23c200ffb9b5512b8e3691116a76d1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-08-23 13:45:07 +0900

    💄 タグの見た目を変更

--- src/features/navigation/components/TagLinks/TabLink/index.tsx
+++ src/features/navigation/components/TagLinks/TabLink/index.tsx
@@ -1,24 +1,18 @@
-import type { StaticImageData } from "next/image";
-import Image from "next/image";
 import Link from "next/link";
 import BudouX from "@/shared/components/BudouX";
 
 type TagLinkProps = {
   name: string;
   href: string;
-  image: string | StaticImageData;
 };
 
-export default function TagLink({ name, href, image }: TagLinkProps) {
+export default function TagLink({ name, href }: TagLinkProps) {
   return (
     <Link
-      className="interactive-scale-brightness relative flex h-8 pc:h-10 shrink-0 items-center justify-center overflow-hidden rounded-md bg-warm-black-50 px-4 py-2 text-center"
+      className="interactive-scale-brightness relative flex h-8 shrink-0 items-center justify-center overflow-hidden rounded-md bg-warm-black-50 px-4 py-2 text-center text-warm-white text-xs"
       href={href}
     >
-      <Image className="absolute inset-0 object-cover blur-xs brightness-70" src={image} alt={`タグ: ${name}`} fill />
-      <p className="z-1 text-warm-white text-xs">
-        <BudouX>{`#${name}`}</BudouX>
-      </p>
+      <BudouX>{`#${name}`}</BudouX>
     </Link>
   );
 }
--- src/features/navigation/components/TagLinks/index.tsx
+++ src/features/navigation/components/TagLinks/index.tsx
@@ -1,4 +1,3 @@
-import type { StaticImageData } from "next/image";
 import { twMerge } from "tailwind-merge";
 import IconTag from "@/assets/icons/tag.svg";
 import TagLink from "./TabLink";
@@ -6,7 +5,6 @@ import TagLink from "./TabLink";
 type Tag = {
   name: string;
   href: string;
-  image: string | StaticImageData;
 };
 
 type TagLinksProps = {
@@ -17,12 +15,12 @@ type TagLinksProps = {
 export default function TagLinks({ tags, className }: TagLinksProps) {
   return (
     <div className={twMerge("flex gap-2 overflow-x-scroll py-1", className)}>
-      <div className="flex h-8 pc:h-10 shrink-0 items-center justify-center gap-1 rounded-md bg-warm-black p-2 px-4 text-center text-warm-white">
+      <div className="flex h-8 shrink-0 items-center justify-center gap-1 rounded-md bg-warm-black p-2 px-4 text-center text-warm-white">
         <IconTag className="h-3 w-3" />
         <p className="text-xs">タグ</p>
       </div>
       {tags.map((tag) => (
-        <TagLink key={tag.name} name={tag.name} href={tag.href} image={tag.image} />
+        <TagLink key={tag.name} name={tag.name} href={tag.href} />
       ))}
     </div>
   );
