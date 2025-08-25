0940b6dedd3bd3179f7de2a47fec1da0e2a2dd0b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-08-25 13:09:49 +0900

    👍 (db/schema): useridをnanoidに

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,15 +1,22 @@
 import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { nanoid } from "nanoid";
 
 export const user = sqliteTable("user", {
-  id: text("id").primaryKey(),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   emailVerified: integer("emailVerified", { mode: "boolean" }).notNull().default(false),
   image: text("image"),
+  plan: text("plan").notNull().default("free"),
+  maxPhotos: integer("maxPhotos").notNull().default(1000),
+  uploadedPhotos: integer("uploadedPhotos").notNull().default(0),
   createdAt: integer("createdAt", { mode: "timestamp" }).notNull().default(sql`(unixepoch())`),
   updatedAt: integer("updatedAt", { mode: "timestamp" })
     .notNull()
+
     .default(sql`(unixepoch())`)
     .$onUpdate(() => new Date()),
 });
