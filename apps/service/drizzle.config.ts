a3e96e85f47032b42e4ca13329a2f4bedb2d5058
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-08-30 22:41:33 +0900

    💥 DBスキーマをパッケージに分離

--- apps/service/drizzle.config.ts
+++ apps/service/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema/index.ts",
-  out: "./migrations",
+  schema: "../../packages/service-db/src/schema/index.ts",
+  out: "../../packages/service-db/src/migrations",
   dialect: "sqlite",
   dbCredentials: {
     url: process.env.DATABASE_PATH || "",
--- apps/service/src/lib/auth.ts
+++ apps/service/src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
-import { getDB } from "../db";
+import { getDB } from "./db";
 
 export const getAuth = (db: D1Database) => {
   return betterAuth({
