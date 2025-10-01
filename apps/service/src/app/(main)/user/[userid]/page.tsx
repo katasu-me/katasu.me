9d08841bff5473ca75590f2a89d57b806aa6b6bf
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-01 18:39:44 +0900

    🚧 (service): 投稿画像を表示

--- apps/service/src/app/(main)/user/[userid]/page.tsx
+++ apps/service/src/app/(main)/user/[userid]/page.tsx
@@ -1,5 +1,6 @@
+import { getImagesByUserId } from "@katasu.me/service-db";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { notFound } from "next/navigation";
-import type { ComponentProps } from "react";
 import { fallback, object, parse } from "valibot";
 import { fetchUserWithCache } from "@/actions/user";
 import IconDots from "@/assets/icons/dots.svg";
@@ -7,8 +8,8 @@ import IconSearch from "@/assets/icons/search.svg";
 import Header from "@/components/Header";
 import IconButton from "@/components/IconButton";
 import TagLinks from "@/components/Navigation/TagLinks";
-import type FrameImage from "@/features/gallery/components/FrameImage";
 import GalleryView, { GalleryViewSchema } from "@/features/gallery/components/GalleryView";
+import { toFrameImageProps } from "@/features/gallery/lib/convert";
 
 const tags = [
   {
@@ -39,57 +40,25 @@ export default async function UserPage({ params, searchParams }: PageProps<"/use
     notFound();
   }
 
+  const { env } = getCloudflareContext();
   const { view } = parse(searchParamsSchema, await searchParams);
 
-  const images: ComponentProps<typeof FrameImage>[] = [
-    {
-      id: "1",
-      src: "/dummy/a.avif",
-      alt: "画像",
-      width: 2624,
-      height: 3936,
-      title: "縦長の画像",
-      linkParams: {
-        userId: user.id,
-        imageId: "1",
-      },
-    },
-    {
-      id: "2",
-      src: "/dummy/b.avif",
-      alt: "画像",
-      width: 2560,
-      height: 1440,
-      title: "横長の風景",
-      linkParams: {
-        userId: user.id,
-        imageId: "2",
-      },
-    },
-    {
-      id: "3",
-      src: "/dummy/c.avif",
-      alt: "画像",
-      width: 1440,
-      height: 2560,
-      title: "正方形",
-      linkParams: {
-        userId: user.id,
-        imageId: "3",
-      },
-    },
-    {
-      id: "4",
-      src: "/dummy/d.avif",
-      alt: "画像",
-      width: 2560,
-      height: 1440,
-      linkParams: {
-        userId: user.id,
-        imageId: "4",
-      },
-    },
-  ];
+  // TODO: ここらへんを関数に切って 'use cache' でキャッシュしたい
+  const getImagesResult = await getImagesByUserId(env.DB, userid, {
+    offset: 0,
+    order: "desc",
+  });
+
+  console.log(getImagesResult);
+
+  if (!getImagesResult.success) {
+    console.error("Failed to fetch images:", getImagesResult.error);
+    notFound(); // FIXME: 500を返す
+  }
+
+  const images = getImagesResult.data.map((image) => toFrameImageProps(image, userid));
+
+  // TODO: EnptyStateがないのでつくる
 
   return (
     <div className="col-span-full grid grid-cols-subgrid gap-y-12 py-16">
