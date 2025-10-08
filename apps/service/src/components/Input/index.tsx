134fd5d39a57ee106e21b7694ce1522e24a0943b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-08 23:32:28 +0900

    💄 (service): カウンターの位置を変更

--- apps/service/src/components/Input/index.tsx
+++ apps/service/src/components/Input/index.tsx
@@ -11,10 +11,9 @@ type Props = {
 
 export default function Input({ maxLength, currentLength, className, label, error, ...props }: Props) {
   const length = currentLength ?? props.value?.toString().length ?? 0;
-  const hasBottom = error || maxLength;
 
   return (
-    <div className={twMerge("w-full", className)}>
+    <div className={twMerge("relative w-full", className)}>
       {label && (
         <label htmlFor={props.id} className="mb-2 block font-medium text-sm text-warm-black">
           {label}
@@ -23,16 +22,16 @@ export default function Input({ maxLength, currentLength, className, label, erro
       <input
         {...props}
         maxLength={maxLength}
-        className="w-full rounded-md border border-warm-black-50 bg-warm-white px-3 py-2 text-warm-black placeholder:text-warm-black-50 focus:border-warm-black focus:outline-none disabled:cursor-not-allowed disabled:bg-warm-black-10"
+        className="w-full rounded-md border border-warm-black-50 bg-warm-white py-2 pr-16 pl-3 text-warm-black placeholder:text-warm-black-50 focus:border-warm-black focus:outline-none disabled:cursor-not-allowed disabled:bg-warm-black-10"
       />
-      {hasBottom && (
+      {maxLength && (
+        <span className="absolute right-2 bottom-1 text-sm text-warm-black-100">
+          {length}/{maxLength}
+        </span>
+      )}
+      {error && (
         <div className="mt-1 flex items-center justify-end">
           {error && <p className="flex-1 text-red-600 text-sm">{error}</p>}
-          {maxLength && (
-            <span className="text-sm text-warm-black-100">
-              {length}/{maxLength}
-            </span>
-          )}
         </div>
       )}
     </div>
