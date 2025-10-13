d0a68670ac87bf6c4ebbe0e2038efd615744e1fa
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-13 13:38:41 +0900

    💄 (service): ちょっと

--- apps/service/src/components/ErrorPage/index.tsx
+++ apps/service/src/components/ErrorPage/index.tsx
@@ -14,7 +14,7 @@ export default function ErrorPage({ title, children, showBackButton = false }: E
     <div className="col-start-2 flex min-h-screen flex-col items-center justify-center">
       <section className="flex flex-col items-center justify-center text-center">
         <LogoImage className="pc:w-48 w-32 opacity-70" />
-        <h1 className="mt-8 font-normal pc:text-3xl text-2xl text-warm-black tracking-[0.1em]">{title}</h1>
+        <h1 className="mt-8 font-normal pc:text-3xl text-2xl text-warm-black">{title}</h1>
         <div className="mt-4 text-warm-black">{children}</div>
         {showBackButton && (
           <Link href="/" className="mt-8">
