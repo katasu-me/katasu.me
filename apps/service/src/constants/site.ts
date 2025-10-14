85d56fe3be123ec4606048c025df687ba18cacfd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-14 21:43:34 +0900

    ♻️ (service): getBaseUrl()をBASE_URL定数に変更し、SITE_URLと統合

--- apps/service/src/constants/site.ts
+++ apps/service/src/constants/site.ts
@@ -1,5 +1,4 @@
 export const SITE_NAME = "katasu.me (かたすみ)";
-export const SITE_URL = "https://katasu.me";
 
 export const SITE_DESCRIPTION_SHORT = "「じぶん」のための、インターネットのかたすみ";
 export const SITE_DESCRIPTION_LONG = `${SITE_NAME}はインターネットのかたすみにある、ぽつんと画像をおいておける場所です。`;
@@ -8,3 +7,8 @@ export const DOCS_INFORMATION = "https://docs.katasu.me/info";
 export const DOCS_TERMS_OF_SERVICE = "https://docs.katasu.me/others/terms";
 export const DOCS_PRIVACY_POLICY = "https://docs.katasu.me/others/privacy";
 export const DOCS_LICENSE = "https://docs.katasu.me/others/license/";
+
+/**
+ * 環境に応じたベースURL
+ */
+export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
