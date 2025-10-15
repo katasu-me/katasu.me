c5841f28a298307ce1d6bcf2b7b2f4cc8d0bc174
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-16 01:11:10 +0900

    ♻️ (service): ハードコードされたURLを環境変数に変更

--- apps/service/src/app/layout.tsx
+++ apps/service/src/app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
   return (
     <html lang="ja">
       <head>
-        <link rel="preconnect" href="https://static.katasu.me" />
+        <link rel="preconnect" href={process.env.NEXT_PUBLIC_IMAGE_R2_URL} />
       </head>
       <body
         className={twMerge(
