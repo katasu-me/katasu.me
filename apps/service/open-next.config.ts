b6dd2eb3a9f36206ad8d8eb9d71ebefc6b24fcbb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-16 03:19:28 +0900

    🔧 (service): おためし

--- apps/service/open-next.config.ts
+++ apps/service/open-next.config.ts
@@ -1,4 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import { purgeCache } from "@opennextjs/cloudflare/overrides/cache-purge/index";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
@@ -7,8 +8,26 @@ import doShardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sha
 export default defineCloudflareConfig({
   incrementalCache: withRegionalCache(r2IncrementalCache, {
     mode: "long-lived",
+    // Setting `bypassTagCacheOnCacheHit` to `true` requires enabling cache purge
     bypassTagCacheOnCacheHit: true,
   }),
-  tagCache: doShardedTagCache({ baseShardSize: 12 }),
   queue: doQueue,
+  // This is only required if you use On-demand revalidation
+  tagCache: doShardedTagCache({
+    baseShardSize: 12,
+    regionalCache: true, // Enable regional cache to reduce the load on the DOs
+    regionalCacheTtlSec: 5, // The TTL for the regional cache
+    regionalCacheDangerouslyPersistMissingTags: true, // Enable this to persist missing tags in the regional cache
+    shardReplication: {
+      numberOfSoftReplicas: 4,
+      numberOfHardReplicas: 2,
+      regionalReplication: {
+        defaultRegion: "enam",
+      },
+    },
+  }),
+  // Disable this if you want to use PPR
+  enableCacheInterception: true,
+  // you can also use the `durableObject` option to use a durable object as a cache purge
+  cachePurge: purgeCache({ type: "direct" }),
 });
