83c6d18f8ae4ec0d5358e81b62851524812abc49
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-11-30 22:26:51 +0900

    ✨ (service): faviconとか

--- apps/service/src/libs/meta.ts
+++ apps/service/src/libs/meta.ts
@@ -15,31 +15,71 @@ export const generateMetadata = (
 ): DetailedHTMLProps<MetaHTMLAttributes<HTMLMetaElement>, HTMLMetaElement>[] => {
   const title = options?.pageTitle ? `${options.pageTitle} | ${SITE_NAME}` : `${SITE_NAME} | ${SITE_DESCRIPTION_SHORT}`;
   const description = options?.description || SITE_DESCRIPTION_SHORT;
-  const twitterCard = options?.twitterCard || (options?.imageUrl ? "summary_large_image" : "summary");
+  const twitterCard = options?.twitterCard || "summary_large_image";
 
   const metaTags: DetailedHTMLProps<MetaHTMLAttributes<HTMLMetaElement>, HTMLMetaElement>[] = [
     { title },
-    { name: "description", content: description },
-    { property: "og:title", content: title },
-    { property: "og:description", content: description },
-    { name: "twitter:card", content: twitterCard },
-    { name: "twitter:title", content: title },
-    { name: "twitter:description", content: description },
-    { name: "robots", content: options?.noindex ? "noindex,nofollow" : "index,follow" },
+    {
+      name: "description",
+      content: description,
+    },
+    {
+      property: "og:title",
+      content: title,
+    },
+    {
+      property: "og:description",
+      content: description,
+    },
+    {
+      name: "twitter:card",
+      content: twitterCard,
+    },
+    {
+      name: "twitter:title",
+      content: title,
+    },
+    {
+      name: "twitter:description",
+      content: description,
+    },
+    {
+      name: "robots",
+      content: options?.noindex ? "noindex,nofollow" : "index,follow",
+    },
   ];
 
   if (options?.path) {
-    metaTags.push({ property: "og:url", content: options.path });
-    metaTags.push({ name: "twitter:url", content: options.path });
-  }
+    const content = new URL(options.path, import.meta.env.VITE_SITE_URL).toString();
 
-  if (options?.imageUrl) {
     metaTags.push(
-      { property: "og:image", content: options.imageUrl },
-      { property: "og:image:alt", content: options.pageTitle || SITE_NAME },
-      { name: "twitter:image", content: options.imageUrl },
+      {
+        property: "og:url",
+        content,
+      },
+      {
+        name: "twitter:url",
+        content,
+      },
     );
   }
+
+  const imageUrl = options?.imageUrl || new URL("/og-image.png", import.meta.env.VITE_SITE_URL).toString();
+
+  metaTags.push(
+    {
+      property: "og:image",
+      content: imageUrl,
+    },
+    {
+      property: "og:image:alt",
+      content: options?.pageTitle || SITE_NAME,
+    },
+    {
+      name: "twitter:image",
+      content: imageUrl,
+    },
+  );
 
   return metaTags;
 };
--- apps/service/src/routes/__root.tsx
+++ apps/service/src/routes/__root.tsx
@@ -29,14 +29,13 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       ...generateMetadata(),
     ],
     links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-      {
-        rel: "preconnect",
-        href: import.meta.env.VITE_IMAGE_R2_URL,
-      },
+      { rel: "stylesheet", href: appCss },
+      { rel: "preconnect", href: import.meta.env.VITE_IMAGE_R2_URL },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "icon", href: "/favicon.ico" },
     ],
   }),
 
