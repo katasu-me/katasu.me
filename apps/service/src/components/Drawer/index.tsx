13487c44d1a85024cbbf44903f5272b471dc3736
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2026-02-21 16:05:33 +0900

    🚑 (service) なんかドロワーのスタイルを修正

--- apps/service/src/components/Drawer/index.tsx
+++ apps/service/src/components/Drawer/index.tsx
@@ -13,15 +13,15 @@ type Props = {
 
 export default function Drawer({ title, children, triggerChildren, titleClassname, innerClassname, ...props }: Props) {
   return (
-    <VaulDrawer.Root {...props} repositionInputs={false}>
+    <VaulDrawer.Root {...props}>
       {triggerChildren && <VaulDrawer.Trigger asChild>{triggerChildren}</VaulDrawer.Trigger>}
       <VaulDrawer.Portal>
         <VaulDrawer.Overlay className="fixed inset-0 z-floating bg-black/40" />
 
-        <VaulDrawer.Content className="fixed right-0 bottom-0 left-0 z-floating m-2 pc:m-4 h-fit overflow-hidden rounded-xl bg-warm-white p-8 pc:px-4 pt-4 outline-none">
-          <VaulDrawer.Handle />
+        <VaulDrawer.Content className="fixed right-0 bottom-0 left-0 z-floating m-2 pc:m-4 flex max-h-[calc(100dvh-1rem)] pc:max-h-[calc(100dvh-2rem)] flex-col overflow-hidden rounded-xl bg-warm-white p-8 pc:px-4 pt-4 outline-none">
+          <VaulDrawer.Handle className="shrink-0" />
 
-          <div className={twMerge("mx-auto mt-6 max-w-md", innerClassname)}>
+          <div className={twMerge("mx-auto mt-6 max-w-md overflow-y-auto", innerClassname)}>
             <VaulDrawer.Title className={twMerge("mb-6 text-warm-black text-xl", titleClassname)}>
               {title}
             </VaulDrawer.Title>
